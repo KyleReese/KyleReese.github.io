d6a94a6b38e33fdb476ea7faa437d3ae21a03614
Kyle Reese <KyleReese@users.noreply.github.com>
2025-02-18 01:35:07 -0700

    second try for github actions deploy

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
-  base: "/KyleReese.github.io/",
+  base: "/",
 });
