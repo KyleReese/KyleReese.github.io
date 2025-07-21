3aabe56fe7366588c7e1309a3d36e4c384f316f4
Kyle Reese <KyleReese@users.noreply.github.com>
2025-07-20 22:14:31 -0600

    Remove empty about and skills pages for now

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -76,8 +76,8 @@ const isMobileMenuOpen = ref<boolean>(false);
 const navItems: NavItem[] = [
   { name: "Home", path: "/" },
   { name: "Projects", path: "/projects" },
-  { name: "Skills", path: "/skills" },
-  { name: "About", path: "/about" },
+  // { name: "Skills", path: "/skills" },
+  // { name: "About", path: "/about" },
   { name: "Contact", path: "/contact" },
 ];
 </script>
